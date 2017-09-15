3ab37d792b693358abc60d64aec866f472f8ce27
Ethan.Walters <Ethan.Walters@mu-c1-03.mullen.pvt.k12.co.us>
2017-09-15 14:37:51 -0600

    made a format!!

--- Concentration 2.0/src/PlayGame.java
+++ Concentration 2.0/src/PlayGame.java
@@ -8,55 +8,55 @@ public class PlayGame
 						
 						String [] [] concentrationGame = new String [4][4];
 						
-						concentrationGame [0][0] = " |   pipes   | ";
-						concentrationGame [0][1] = " |  metallic | ";
-						concentrationGame [0][2] = " |   gloomy  | ";
-						concentrationGame [0][3] = " |    half   | ";
-						concentrationGame [1][0] = " | discovery | ";
-						concentrationGame [1][1] = " |   gadget  | ";
-						concentrationGame [1][2] = " | discovery | ";
-						concentrationGame [1][3] = " |   scared  | ";
-						concentrationGame [2][0] = " |   gloomy  | ";
-						concentrationGame [2][1] = " |   pipes   | ";
-						concentrationGame [2][2] = " |   scared  | ";
-						concentrationGame [2][3] = " |   actor   | ";
-						concentrationGame [3][0] = " | discovery | ";
-						concentrationGame [3][1] = " |   half    | ";
-						concentrationGame [3][2] = " |   gadget  | ";
-						concentrationGame [3][3] = " |  metallic | ";
+						WhatTheUserSees.allTheBoxes();
 						
-						System.out.println("         We're going to play a concentration game!");
+//						concentrationGame [0][0] = " |   pipes   | ";
+//						concentrationGame [0][1] = " |  metallic | ";
+//						concentrationGame [0][2] = " |   gloomy  | ";
+//						concentrationGame [0][3] = " |    half   | ";
+//						concentrationGame [1][0] = " | discovery | ";
+//						concentrationGame [1][1] = " |   gadget  | ";
+//						concentrationGame [1][2] = " | discovery | ";
+//						concentrationGame [1][3] = " |   scared  | ";
+//						concentrationGame [2][0] = " |   gloomy  | ";
+//						concentrationGame [2][1] = " |   pipes   | ";
+//						concentrationGame [2][2] = " |   scared  | ";
+//						concentrationGame [2][3] = " |   actor   | ";
+//						concentrationGame [3][0] = " | discovery | ";
+//						concentrationGame [3][1] = " |   half    | ";
+//						concentrationGame [3][2] = " |   gadget  | ";
+//						concentrationGame [3][3] = " |  metallic | ";
+						
+						System.out.println("     We're going to play a concentration game!");
 						
 						
-						System.out.println("        0              1              2              3");
-						
-						for(int row = 0 ; row < concentrationGame.length ; row ++){
-							
-							System.out.print(row);
-							
-							for(int col = 0 ; col < concentrationGame[0].length ; col ++){
-								if(row == 0){
-									
-									System.out.print(concentrationGame [row] [col]);
-									
-								}
-								else if(row == 1){
-									
-									System.out.print(concentrationGame [row] [col]);
-								}
-								else if(row == 2){
-									
-									System.out.print(concentrationGame [row] [col]);
-								}
-								else if(row == 3){
-									
-									System.out.print(concentrationGame [row] [col]);
-								}
-								
-							}
-							
-							System.out.println();
-						}
+//						for(int row = 0 ; row < concentrationGame.length ; row ++){
+//							
+//							System.out.print(row);
+//							
+//							for(int col = 0 ; col < concentrationGame[0].length ; col ++){
+//								if(row == 0){
+//									
+//									System.out.print(concentrationGame [row] [col]);
+//									
+//								}
+//								else if(row == 1){
+//									
+//									System.out.print(concentrationGame [row] [col]);
+//								}
+//								else if(row == 2){
+//									
+//									System.out.print(concentrationGame [row] [col]);
+//								}
+//								else if(row == 3){
+//									
+//									System.out.print(concentrationGame [row] [col]);
+//								}
+//								
+//							}
+//							
+//							System.out.println();
+//						}
 						
 						Scanner userInput = new Scanner(System.in);
 						
---
+++ Concentration 2.0/src/WhatTheUserSees.java
@@ -0,0 +1,24 @@
+
+public class WhatTheUserSees
+	{
+		public static void allTheBoxes(){
+			
+			System.out.println("        1          2         3          4    ");
+			System.out.println("   ___________________________________________");
+			System.out.println("  |          |          |          |          |");
+			System.out.println("A |          |          |          |          |");
+			System.out.println("  |__________|__________|__________|__________|");
+			System.out.println("   ___________________________________________");
+			System.out.println("  |          |          |          |          |");
+			System.out.println("B |          |          |          |          |");
+			System.out.println("  |__________|__________|__________|__________|");
+			System.out.println("   ___________________________________________");
+			System.out.println("  |          |          |          |          |");
+			System.out.println("C |          |          |          |          |");
+			System.out.println("  |__________|__________|__________|__________|");
+			System.out.println("   ___________________________________________");
+			System.out.println("  |          |          |          |          |");
+			System.out.println("D |          |          |          |          |");
+			System.out.println("  |__________|__________|__________|__________|");
+		}
+	}
